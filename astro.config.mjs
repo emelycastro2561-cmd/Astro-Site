0232e1beee5280741a536bb04aa0355dbea33750
Emely C <emely.castro2561@gmail.com>
2026-03-31 23:09:43 -0700

    installed an astro package using the command line and created an RSS feed for my website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://69ccb3dc5a59f80007f5f3f7--emy-cmd-astro-site.netlify.app/"
+});
